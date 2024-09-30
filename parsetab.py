fb5109feb2ef08ff8039402455b92a1a5fa503ef
AquinoSan <aldrin06sanchez@gmail.com>
2024-09-29 23:13:49 -0600

    Update parsetab.py

--- parsetab.py
+++ parsetab.py
@@ -6,7 +6,7 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'DOT EQUALS FOR IDENTIFIER INT LBRACE LESS_EQUAL LPAREN NUMBER OUT PLUS PLUS_PLUS PRINTLN RBRACE RPAREN SEMICOLON STRING SYSTEMprogram : for_loopfor_loop : FOR LPAREN INT IDENTIFIER EQUALS NUMBER SEMICOLON IDENTIFIER LESS_EQUAL NUMBER SEMICOLON increment RPAREN LBRACE statement RBRACEincrement : IDENTIFIER PLUS_PLUS\n                 | IDENTIFIER PLUS EQUALS NUMBERstatement : SYSTEM DOT OUT DOT PRINTLN LPAREN STRING PLUS IDENTIFIER RPAREN SEMICOLON'
+_lr_signature = 'DOT EQUALS FOR IDENTIFIER INT LBRACE LESS_EQUAL LPAREN MENOS NUMBER OUT PLUS PLUS_PLUS PRINTLN RBRACE RPAREN SEMICOLON STRING SYSTEMprogram : for_loopfor_loop : FOR LPAREN INT IDENTIFIER EQUALS NUMBER SEMICOLON IDENTIFIER LESS_EQUAL NUMBER SEMICOLON increment RPAREN LBRACE statement RBRACEincrement : IDENTIFIER PLUS_PLUS\n                 | IDENTIFIER PLUS EQUALS NUMBERstatement : SYSTEM DOT OUT DOT PRINTLN LPAREN STRING PLUS IDENTIFIER RPAREN SEMICOLON'
     
 _lr_action_items = {'FOR':([0,],[3,]),'$end':([1,2,24,],[0,-1,-2,]),'LPAREN':([3,28,],[4,29,]),'INT':([4,],[5,]),'IDENTIFIER':([5,9,13,31,],[6,10,14,32,]),'EQUALS':([6,17,],[7,19,]),'NUMBER':([7,11,19,],[8,12,21,]),'SEMICOLON':([8,12,33,],[9,13,34,]),'LESS_EQUAL':([10,],[11,]),'PLUS_PLUS':([14,],[16,]),'PLUS':([14,30,],[17,31,]),'RPAREN':([15,16,21,32,],[18,-3,-4,33,]),'LBRACE':([18,],[20,]),'SYSTEM':([20,],[23,]),'RBRACE':([22,34,],[24,-5,]),'DOT':([23,26,],[25,27,]),'OUT':([25,],[26,]),'PRINTLN':([27,],[28,]),'STRING':([29,],[30,]),}
 
@@ -27,9 +27,9 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> program","S'",1,None,None,None),
-  ('program -> for_loop','program',1,'p_program','app.py',78),
-  ('for_loop -> FOR LPAREN INT IDENTIFIER EQUALS NUMBER SEMICOLON IDENTIFIER LESS_EQUAL NUMBER SEMICOLON increment RPAREN LBRACE statement RBRACE','for_loop',16,'p_for_loop','app.py',82),
-  ('increment -> IDENTIFIER PLUS_PLUS','increment',2,'p_increment','app.py',86),
-  ('increment -> IDENTIFIER PLUS EQUALS NUMBER','increment',4,'p_increment','app.py',87),
-  ('statement -> SYSTEM DOT OUT DOT PRINTLN LPAREN STRING PLUS IDENTIFIER RPAREN SEMICOLON','statement',11,'p_statement','app.py',92),
+  ('program -> for_loop','program',1,'p_program','app.py',79),
+  ('for_loop -> FOR LPAREN INT IDENTIFIER EQUALS NUMBER SEMICOLON IDENTIFIER LESS_EQUAL NUMBER SEMICOLON increment RPAREN LBRACE statement RBRACE','for_loop',16,'p_for_loop','app.py',83),
+  ('increment -> IDENTIFIER PLUS_PLUS','increment',2,'p_increment','app.py',87),
+  ('increment -> IDENTIFIER PLUS EQUALS NUMBER','increment',4,'p_increment','app.py',88),
+  ('statement -> SYSTEM DOT OUT DOT PRINTLN LPAREN STRING PLUS IDENTIFIER RPAREN SEMICOLON','statement',11,'p_statement','app.py',93),
 ]
